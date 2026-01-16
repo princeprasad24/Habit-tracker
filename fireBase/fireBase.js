2ead11455f3156ccf2b8c1d73b7f99418d59c947
princeprasad24 <venkatdurgaprasad2222@email.com>
2026-01-16 20:22:32 +0530

    first commit

--- fireBase/fireBase.js
+++ fireBase/fireBase.js
@@ -9,15 +9,26 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDe-TKi1nLH4wqp5saHOGR954sWO-2Ynq4",
+//   authDomain: "habit-tracker-667a7.firebaseapp.com",
+//   databaseURL: "https://habit-tracker-667a7-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "habit-tracker-667a7",
+//   storageBucket: "habit-tracker-667a7.firebasestorage.app",
+//   messagingSenderId: "408418670182",
+//   appId: "1:408418670182:web:6772fbcd47a54df9b2ffa7",
+//   measurementId: "G-GW8C0MDNZ6"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDe-TKi1nLH4wqp5saHOGR954sWO-2Ynq4",
-  authDomain: "habit-tracker-667a7.firebaseapp.com",
-  databaseURL: "https://habit-tracker-667a7-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "habit-tracker-667a7",
-  storageBucket: "habit-tracker-667a7.firebasestorage.app",
-  messagingSenderId: "408418670182",
-  appId: "1:408418670182:web:6772fbcd47a54df9b2ffa7",
-  measurementId: "G-GW8C0MDNZ6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
